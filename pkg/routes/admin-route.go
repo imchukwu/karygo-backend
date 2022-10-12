2090eef160ea8298a3f83b0da9bdee73a2b2a4c5
Kayode Oluwasegun <segun@cimspace.com>
2022-10-12 11:01:31 +0100

    admin model, controller and routes added

---
+++ pkg/routes/admin-route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/imchukwu/karygo_backend/pkg/controllers"
+)
+
+var RegisterAdminRoutes = func (r *mux.Router){
+	r.HandleFunc("/admin", controllers.CreateAdmin).Methods("POST")
+	r.HandleFunc("/admin", controllers.GetAdmins).Methods("GET")
+	r.HandleFunc("/admin/{adminId}", controllers.GetAdmin).Methods("GET")
+	r.HandleFunc("/admin/{adminId}", controllers.UpdateAdmin).Methods("PUT")
+	r.HandleFunc("/admin/{admin}", controllers.DeleteAdmin).Methods("DELETE")
+}
