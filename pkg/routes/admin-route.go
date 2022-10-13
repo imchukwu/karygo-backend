531402c028bf35e07dc5d1ab42f5bf96ae3e51e7
Kayode Oluwasegun <segun@cimspace.com>
2022-10-13 10:58:37 +0100

    more admin APIs added

--- pkg/routes/admin-route.go
+++ pkg/routes/admin-route.go
@@ -11,4 +11,6 @@ var RegisterAdminRoutes = func (r *mux.Router){
 	r.HandleFunc("/admin/{adminId}", controllers.GetAdmin).Methods("GET")
 	r.HandleFunc("/admin/{adminId}", controllers.UpdateAdmin).Methods("PUT")
 	r.HandleFunc("/admin/{admin}", controllers.DeleteAdmin).Methods("DELETE")
+	r.HandleFunc("/admin/transactiion", controllers.GetTransactions).Methods("GET")
+	r.HandleFunc("/admin/transaction/{txId}", controllers.GetTransaction).Methods("GET")
 }
