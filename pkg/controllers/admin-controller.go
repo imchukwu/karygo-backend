9e3839c35af8d41fa4d4c39b4a4d404fe4d6f16e
Kayode Oluwasegun <segun@cimspace.com>
2022-10-28 12:32:51 +0100

    typo fixed

--- pkg/controllers/admin-controller.go
+++ pkg/controllers/admin-controller.go
@@ -30,7 +30,7 @@ import (
 func enableCors(w *http.ResponseWriter) {
 (*w).Header().Set("Access-Control-Allow-Origin", "*")
 (*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS") 
-(*w).Header().Set("Access-Control-Allow-Headers:", "Origin, Content-Type, X-Auth-Token, Authorization")
+(*w).Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, Authorization")
 
 }
 // swagger:route GET /admin admins listAdmins
