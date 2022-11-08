ecfd60ba508a8f6e6d402b12aba9141d781f9939
Kayode Oluwasegun <segun@cimspace.com>
2022-11-08 12:17:23 +0100

    billing APIs

---
+++ pkg/controllers/billing-controller.go
@@ -0,0 +1,98 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/imchukwu/karygo_backend/pkg/models"
+	"github.com/imchukwu/karygo_backend/pkg/utils"
+)
+
+func GetBillings(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	billings := models.GetBillings()
+	res, _ := json.Marshal(billings)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBilling(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	vars := mux.Vars(r)
+	billingId := vars["billingId"]
+	Id, err := strconv.ParseInt(billingId, 0, 0)
+	if err != nil {
+		fmt.Println("An error ocurred while parsing value")
+	}
+
+	billingDetail, _ := models.GetBilling(Id)
+	res, _ := json.Marshal(billingDetail)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+
+func CreateBilling(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	createBilling := &models.Billing{}
+	utils.ParseBody(r, createBilling)
+	u := createBilling.CreateBilling()
+	res, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBilling(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	vars := mux.Vars(r)
+	billingId := vars["userId"]
+	Id, err := strconv.ParseInt(billingId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	billing := models.DeleteBilling(Id)
+	res, _ := json.Marshal(billing)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+
+func GetBillingsByUser(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	Id, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("An error ocurred while parsing value")
+	}
+
+	var billingIds []uint
+	var billings []*models.Billing
+		fmt.Print(billings)
+	billingIds = models.GetBillingsByUser(Id)
+		fmt.Print(billingIds)
+	for index := range billingIds{
+		billing, _ := models.GetBilling(Id)
+		billings[index] = billing
+	}
+
+	fmt.Print(billings)
+
+	res, _ := json.Marshal(billingIds)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
---
+++ pkg/routes/billing-route.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/imchukwu/karygo_backend/pkg/controllers"
+)
+
+var RegisterBillingRoutes = func (r *mux.Router){
+	r.HandleFunc("/billing", controllers.CreateBilling).Methods("POST")
+	r.HandleFunc("/billings", controllers.GetBillings).Methods("GET")
+	r.HandleFunc("/billing/{billingId}", controllers.GetBilling).Methods("GET")
+	r.HandleFunc("/billing/{billingId}", controllers.DeleteBilling).Methods("DELETE")
+	r.HandleFunc("/billing/{userId}/user", controllers.GetBillingsByUser).Methods("GET")
+}
