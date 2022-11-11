960df99fcbef22b61392d1a9f0da40119f2ef82d
Kayode Oluwasegun <segun@cimspace.com>
2022-11-11 13:12:19 +0100

    PUT request verb added to billing-route

--- pkg/routes/billing-route.go
+++ pkg/routes/billing-route.go
@@ -9,6 +9,7 @@ var RegisterBillingRoutes = func (r *mux.Router){
 	r.HandleFunc("/billing", controllers.CreateBilling).Methods("POST")
 	r.HandleFunc("/billings", controllers.GetBillings).Methods("GET")
 	r.HandleFunc("/billing/{billingId}", controllers.GetBilling).Methods("GET")
+	r.HandleFunc("/billing/{billingId}", controllers.UpdateBilling).Methods("PUT")
 	r.HandleFunc("/billing/{billingId}", controllers.DeleteBilling).Methods("DELETE")
 	r.HandleFunc("/billing/{userId}/user", controllers.GetBillingsByUser).Methods("GET")
 }
