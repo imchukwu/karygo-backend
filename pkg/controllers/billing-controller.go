4461486938cbc5b340e1ce7e91038447a2d41fb9
Kayode Oluwasegun <segun@cimspace.com>
2022-11-11 12:53:30 +0100

    update function added to billing-controller

--- pkg/controllers/billing-controller.go
+++ pkg/controllers/billing-controller.go
@@ -51,6 +51,47 @@ func CreateBilling(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+func UpdateBilling(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	var UpdateBilling = &models.Billing{}
+	utils.ParseBody(r, UpdateBilling)
+
+	vars := mux.Vars(r)
+	billingId := vars["billingId"]
+	Id, err := strconv.ParseInt(billingId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	billingDetail, db := models.GetBilling(Id)
+	if UpdateBilling.UserId != 0 {
+		billingDetail.UserId = UpdateBilling.UserId
+	}
+	if UpdateBilling.PreviousBalance != 0 {
+		billingDetail.PreviousBalance = UpdateBilling.PreviousBalance
+	}
+	if UpdateBilling.CurrentBalance != 0 {
+		billingDetail.CurrentBalance = UpdateBilling.CurrentBalance
+	}
+	if UpdateBilling.PaymentType != "" {
+		billingDetail.PaymentType = UpdateBilling.PaymentType
+	}
+	if UpdateBilling.Amount != 0 {
+		billingDetail.Amount = UpdateBilling.Amount
+	}
+	if UpdateBilling.Status != "" {
+		billingDetail.Status = UpdateBilling.Status
+	}
+
+
+	db.Save(&billingDetail)
+
+	res, _ := json.Marshal(billingDetail)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
 func DeleteBilling(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
